1cf6bfa70d04e7a8f94ab3287812585582f8a6e4
MohdAhmadRaza <ahmadrazaking7860@gmail.com>
2024-08-14 02:43:46 +0530

    add

---
+++ script.js
@@ -0,0 +1,46 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices =[];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () =>{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name,i)));
+};
+
+voiceSelect.addEventListener("change",()=>{
+    speech.voice = voices[voiceSelect.value];
+});
+
+
+document.querySelector("button").addEventListener("click",()=>{
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// text to voice
+
+// let speech = new SpeechSynthesisUtterance();
+
+// document.querySelector("button").addEventListener("click",()=>{
+//     speech.text = document.querySelector("textarea").value;
+//     window.speechSynthesis.speak(speech);
+// })
